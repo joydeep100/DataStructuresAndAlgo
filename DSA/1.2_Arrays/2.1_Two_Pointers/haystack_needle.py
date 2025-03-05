b4d696efdbac7d7187daeb0cd8e64110229fe9c8
joydeep <45758076+joydeep100@users.noreply.github.com>
2025-03-05 22:06:27 +0530

    Update haystack_needle.py

--- DSA/1.2_Arrays/2.1_Two_Pointers/haystack_needle.py
+++ DSA/1.2_Arrays/2.1_Two_Pointers/haystack_needle.py
@@ -1,19 +1,14 @@
 def strStr(haystack, needle):
-
-    # Find index of first occurance of substring in string
-
+    
     j = 0
-    count = 0
     for i in range(len(haystack) - len(needle)):
 
         if haystack[i] == needle[j]:
-            count += 1
             j += 1
         else:
             j = 0
 
         # now we also want to return the starting index where we find the match
-
         if j == len(needle):
             return True
 
