5d211714850ce38501f8cdfe43200b25e945223d
joydeep das <jdas@akamai.com>
2025-11-09 15:44:11 +0530

    add multiple shift prog

--- DSA/0.4_InterviewPrep/shift_left_right_imp.py
+++ DSA/0.4_InterviewPrep/shift_left_right_imp.py
@@ -23,3 +23,29 @@ def shiftRight(arr):
 
 print(shiftLeft([1, 2, 3, 2, 1, 1, 3, 2, 4, 5, 6]))
 print(shiftRight([1, 2, 3, 2, 1, 1, 3, 2, 4, 5, 6]))
+
+def shit_right_multiple(input_arr, *shift_values):
+    
+    right = len(input_arr) - 1
+    
+    for shift_val in shift_values:
+        
+        left = 0 # because some items will be swapped on to left
+        
+        while left  <= right:
+            
+            if input_arr[right] == shift_val:
+                right -= 1
+            elif input_arr[left] != shift_val:
+                left += 1
+            else:
+                # swap the values
+                input_arr[left], input_arr[right] = input_arr[right], input_arr[left]
+                left += 1
+                right -= 1
+
+    return input_arr
+    
+print(shit_right_multiple([0,1,1,0,3,4,5], 1, 0))
+print(shit_right_multiple([3,0,1,1,0,3,4,5], 1, 0, 3))
+print(shit_right_multiple([9,0,1,1,0,3,4,5,9], 1, 9, 3))
